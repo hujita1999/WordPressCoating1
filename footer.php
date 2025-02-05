1429bad724a8043d1192ff459fb0b7ef3d78420a
hujita1999 <fujitawork1999@gmail.com>
2025-02-05 21:44:35 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+  <footer id="footer" class="wrapper">
+    <ul class="footer-list">
+      <li><a href="https://www.instagram.com/" target="_blank" rel="noopener">INSTAGRAM</a></li>
+      <li><a href="https://x.com/" target="_blank" rel="noopener">TWITTER</a></li>
+      <li><a href="https://www.facebook.com/" target="_blank" rel="noopener">FACEBOOK</a></li>
+    </ul>
+    <p class="copyright">&copy; <?php echo bloginfo('name'); ?></p>
+  </footer>
+
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,58 @@
+<?php
+
+function my_enqueue_styles() {
+  wp_enqueue_style('ress', '//unpkg.com/ress/dist/ress.min.css', array(), false, 'all');
+  wp_enqueue_style('style', get_stylesheet_uri(), array('ress'), false, 'all');
+}
+add_action('wp_enqueue_scripts', 'my_enqueue_styles');
+
+function st_enqueue_scripts() {
+  wp_deregister_script('jquery');
+  wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', array(), '3.5.1', false);
+  wp_enqueue_script('main', get_theme_file_uri('js/main.js'), array('jquery'), false, true);
+}
+add_action('wp_enqueue_scripts', 'st_enqueue_scripts');
+
+add_theme_support('post-thumbnails');
+
+function pagination($pages = '', $range = 2) {
+  $showitems = ($range * 2) + 1;
+
+  global $paged;
+  if(empty($paged)) {
+    $paged = 1;
+  }
+
+  if($pages == '') {
+    global $wp_query;
+    $pages = $wp_query->max_num_pages;
+    if(!$pages) {
+      $pages = 1;
+    }
+  }
+
+  if(1 != $pages) {
+    echo '<div class="pagination">';
+    echo '<ul>';
+
+    if($paged > 1) {
+      echo '<li class="prev"><a href="' . esc_url(get_pagenum_link($paged - 1)) . '">前のページ</a></li>';
+    }
+
+    for ($i=1; $i <= $pages; $i++) {
+      if (1 != $pages &&(!($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems )) {
+        if ($paged == $i) {
+          echo '<li class="active">' .$i. '</li>';
+        } else {
+          echo '<li><a href="' . esc_url(get_pagenum_link($i)) . '">' .$i. '</a></li>';
+        }
+      }
+    }
+
+    if ($paged < $pages) {
+      echo '<li class="next"><a href="' . esc_url(get_pagenum_link($paged + 1)) . '">次のページ</a></li>';
+    }
+    echo '</ul>';
+    echo '</div>';
+  }
+}
